172e74823e81f0126975d1723198ffbeb9bef877
thanhnguyen98 <ngtruongthanh98@gmail.com>
2021-11-21 10:56:38 +0700

    Connect to ApolloServer, build schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,10 @@ import express from 'express';
 import { createConnection } from 'typeorm';
 import { User } from './entities/User';
 import { Post } from './entities/Post';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 
 const main = async () => {
   await createConnection({
@@ -17,6 +21,19 @@ const main = async () => {
   });
 
   const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+    context: ({ req, res }) => ({ req, res }),
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app, cors: false });
 
   app.listen(4000, () => {
     console.log('Server started on port 4000!');
